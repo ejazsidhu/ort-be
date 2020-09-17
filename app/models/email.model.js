0b2671242efd1a553aea4aeeccccdfac28e58119
Muhammad Ejaz Ur Rehman <muhammad.ejaz@systemsltd.com>
2020-09-17 13:01:31 +0500

    email modal conrollet and router added

---
+++ app/models/email.model.js
@@ -0,0 +1,34 @@
+
+const UserSchema = mongoose.Schema({
+    userName: {
+        type: String,
+        required: true,
+        trim: true
+    },
+ 
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        validate: value => {
+            if (!validator.isEmail(value)) {
+                throw new Error({ error: 'Invalid Email address' })
+            }
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: 7
+    },
+    tokens: [{
+        token: {
+            type: String,
+            required: true
+        }
+    }],
+    
+}, {
+    timestamps: true
+});
---
+++ app/routes/email.routes.js
@@ -0,0 +1,35 @@
+module.exports = (app) => {
+    const email = require('../controllers/user.controller.js');
+    const auth = require('../middleware/auth');
+    /**
+     * 
+     * 
+     * Routes for USERS
+     * Use auth function to secure the endpont with jwt token
+     */
+    // Create a new User
+    // if you dont have a user, to create the user, take out auth function 
+    // so that it lets you create your first user for tests
+    //app.post('/users/create', users.create);
+    app.post('/email/create', auth, email.create);
+
+    // Retrieve all email
+    app.get('/email', email.findAll);
+
+    // Retrieve a single User with userId
+    app.get('/email/:userId', auth, email.findOne);
+
+    // Update a User with userId
+    // make sure you pass id in body and in url of request
+    app.post('/email/update/:userId', auth, email.update);
+
+    // Delete a User with noteId
+    app.get('/email/delete/:userId', auth, email.delete);
+
+    // Delete a User with noteId
+    app.post('/email/bulkdelete', auth, email.bulkDelete);
+
+
+
+
+}
