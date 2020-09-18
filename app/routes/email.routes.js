d993e9fcfb21c0e2ab2a42477ee1f2d162f53afc
ejazsidhu <ejazsidhu@gmail.com>
2020-09-19 01:11:49 +0500

    file cose error resolved

--- app/routes/email.routes.js
+++ app/routes/email.routes.js
@@ -1,3 +1,9 @@
+const multer = require("multer");
+const upload = multer({
+    dest: "/public"
+    // you might also want to set some limits: https://github.com/expressjs/multer#limits
+  });
+
 module.exports = (app) => {
     const email = require('../controllers/email.controller.js');
     const auth = require('../middleware/auth');
@@ -31,7 +37,35 @@ module.exports = (app) => {
     // // Delete a User with noteId
     // app.post('/email/bulkdelete', auth, email.bulkDelete);
 
-
+    app.post(
+        "/upload",
+        upload.single("file" /* name attribute of <file> element in your form */),
+        (req, res) => {
+            console.log("files",req.file)
+          const tempPath = req.file.path;
+          const targetPath = path.join(__dirname, "./uploads/image.png");
+      
+          if (path.extname(req.file.originalname).toLowerCase() === ".png") {
+            fs.rename(tempPath, targetPath, err => {
+              if (err) return handleError(err, res);
+      
+              res
+                .status(200)
+                .contentType("text/plain")
+                .end("File uploaded!");
+            });
+          } else {
+            fs.unlink(tempPath, err => {
+              if (err) return handleError(err, res);
+      
+              res
+                .status(403)
+                .contentType("text/plain")
+                .end("Only .png files are allowed!");
+            });
+          }
+        }
+      );
 
 
 }
